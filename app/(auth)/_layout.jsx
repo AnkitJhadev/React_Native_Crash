f16d000ebc38091067fc7d956d479b955d158a3d
AnkitJhadev <jha80250@gmail.com>
2024-10-15 22:25:22 +0530

    Created sign-in and sign-up screen

--- app/(auth)/_layout.jsx
+++ app/(auth)/_layout.jsx
@@ -1,14 +1,30 @@
-import { StyleSheet, Text, View } from 'react-native'
-import React from 'react'
+import { Redirect, Stack } from "expo-router";
+import { StatusBar } from "expo-status-bar";
+
 
 const AuthLayout = () => {
+  
+
   return (
-    <View>
-      <Text>_layout</Text>
-    </View>
-  )
-}
+    <>
+      <Stack>
+        <Stack.Screen
+          name="sign-in"
+          options={{
+            headerShown: false,
+          }}
+        />
+        <Stack.Screen
+          name="sign-up"
+          options={{
+            headerShown: false,
+          }}
+        />
+      </Stack>
 
-export default AuthLayout
+  
+    </>
+  );
+};
 
-const styles = StyleSheet.create({})
+export default AuthLayout;
